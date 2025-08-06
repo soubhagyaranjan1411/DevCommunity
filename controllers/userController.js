ace17bb862b495f9d34d9b9fa87d010441a84eb0
Soubhagya Ranjan <soubhagyaranjan1411@gmail.com>
2025-08-06 14:54:44 +0530

    Implement JWT and update routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,9 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const generateToken = (user) =>{
+    return jwt.sign({user},process.env.JWT_SECRET);
+}
 
 const registerUser = async (req,res) =>{
     const { firstName, lastName, emailId, password} = req.body;
@@ -25,14 +30,42 @@ const registerUser = async (req,res) =>{
     });
 
     await newUser.save();
+    const token = generateToken(newUser);
+
+
     
-    res.status(201).json("USER CREATED",{newUser});
+    res.status(201).json({
+        message: "USER ADDED SUCCESSFULLY",
+        token
+    });
     
 }
 
 
-// const loginUser = () => {
+const loginUser = async (req,res) => {
+    const { emailId, password} = req.body;
 
-// }
+    //VALIDATION
 
-module.exports = { registerUser }
+    if (!emailId || ! password){
+       return res.status(400).json({message: "ADD ALL DETAILS"});
+    }
+
+    const userExists = await User.findOne({emailId});
+    console.log(userExists);
+
+    if (!userExists){
+        return res.status(400).json({message: "No user Found"});
+    }
+
+    if (req.body.password != userExists.password){
+        console.log(req.body.password);
+        return res.status(400).json({message: "Incorrect Password"});
+    }
+    const token = generateToken(userExists);
+
+    return res.status(200).json({message: "LoggedIn", token});
+    
+}
+
+module.exports = { registerUser, loginUser }
